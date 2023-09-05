59227c655c30993a1ff2f1005b1df0dbf55761bf
pallavikanki <132667720+pallavikanki@users.noreply.github.com>
2023-09-05 15:41:07 +0530

    fi

--- pages/FormulaForm.js
+++ pages/FormulaForm.js
@@ -1,182 +1,120 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+//import Structure from './Structure'
+const FormulaForm = () => {
+  const [sum, setSum] = useState(0);
+  const [result, setResult] = useState(0);
+  const [multi, setMulti] = useState(0);
+  const [template, setTemplate] = useState([])
+  const [formulaName, setFormulaName] = useState('')
+  const [formula, setFormula] = useState('')
+  useEffect(() => {
+    const fetchFormula = async () => {
+      let formulaTemplate = JSON.parse(localStorage.getItem("formulaTemplate"));
 
-function FormulaForm() {
-  const [formulaName, setFormulaName] = useState('');
-  const [formula, setFormula] = useState('');
-  const [template, setTemplate] = useState([]);
-  const [errors, setErrors] = useState({ template: [] });
-
-  const handleNumRowsChange = (event) => {
-    let tmpArr = [];
-    for (let i = 0; i < event.target.value; i++) {
-      let tmpObj = {
-        id: i,
-        key: '',
-        label: '',
-        limit: '',
-        unit: '',
-        defaultValue: ''
-      }
-      tmpArr.push(tmpObj)
+      let formulaName = localStorage.getItem("formula Name");
+      let formula = localStorage.getItem("formula");
+      setTemplate(formulaTemplate)
+      setFormulaName(formulaName)
+      setFormula(formula)
     }
-    setTemplate(tmpArr);
-    console.log(tmpArr);
-  };
-
-  const updateValue = (e, updateFor, id) => {
-    console.log("Value" + e.target.value);
-    console.log('Update for' + updateFor);
-    console.log('id' + id);
-
-    template[id][updateFor] = e.target.value;
-    setTemplate([...template]);
-    validateTemplate();
+    
+    fetchFormula();
+  }, [])
+  const textChangeEvent = (e, item) => {
+    template[item.id].value = parseFloat(e.target.value) ? parseFloat(e.target.value) : 0;
+    setTemplate(template)
     console.log(JSON.stringify(template))
+    calculateSum();
+    calculateMulti();
+   
   }
-
-  const validateTemplate = () => {
-    let valid = true;
-    const newTemplateErrors = [];
-
-    template.forEach((item, index) => {
-      const itemErrors = {};
-      if (!item.key.trim()) {
-        itemErrors.key = 'Key is required';
-        valid = false;
-      }
-      if (!item.label.trim()) {
-        itemErrors.label = 'Label is required';
-        valid = false;
-      }
-      if (!item.limit.trim()) {
-        itemErrors.limit = 'Limit is required';
-        valid = false;
-      }
-      if (!item.unit.trim()) {
-        itemErrors.unit = 'Unit is required';
-        valid = false;
-      }
-      if (!item.defaultValue.trim()) {
-        itemErrors.defaultValue = 'DefaultValue is required';
-        valid = false;
-      }
-
-      newTemplateErrors.push(itemErrors);
-    });
-
-    setErrors({ ...errors, template: newTemplateErrors });
-    return valid;
-  }
-
-  const handleSubmit = () => {
-    const isTemplateValid = validateTemplate();
-  
-    if (isTemplateValid && formulaName.trim() && formula.trim()) {
-      console.log("Form is valid");
-      localStorage.setItem("formulaTemplate", JSON.stringify(template));
-      localStorage.setItem("formula", formula);
-      localStorage.setItem("formula Name", formulaName);
-
-    }
-  }
-  const renderError = (field, index) => {
-    const error = errors.template[index] && errors.template[index][field];
-    return error ? <div className="text-red-500">{error}</div> : null;
+  // Use Object.values() to get an array of values from the JSON object
+  const value = Object.values(template);
+  // Calculate the sum of values and update the state
+  const calculateSum = () => {
+    const total = value.reduce((acc, curr) => (acc ? acc : 0) + curr.value, 0);
+    setSum(total);
   };
+  const calculateMulti = () => {
+    const m = value.reduce((acc, curr) => (acc ? acc : 1) * curr.value, 0);
+    setMulti(m);
+  };
+  const jsonString = JSON.stringify(template);
+  console.log(jsonString)
+  function isStringInJsonObjectKey(formula, jsonObject) {
+    let m = formula;
+    {
+      template.length > 0 && (
+        template.map((item) => {
+          console.log(item.key)
+          console.log(formula.includes(item.key))
+          if (formula.includes(item.key)) {
+            m = m.replace(item.key, item.value)
+            console.log("ghkg" + m)
+            localStorage.setItem("formul", m);
+            
+          }
+        }
+        ))
+    }
+    
+    return false; // No match found
+   
+  }
+
+
+
+  if (isStringInJsonObjectKey(formula, jsonString)) {  
+    console.log(`String "${formula}" matches a JSON object key.`);
+  } else {
+    console.log(`String "${formula}" does not match any JSON object key.`);
+  }
+  const handleCalculate = () => {
+    try {
+      let field = localStorage.getItem("formul");
+      setResult(eval(field).toString());
+    } catch (error) {
+      setResult('0');
+    }
+  };
+
+
 
   return (
-    <div className="m-5">
-      <div className='flex flex-col sm:w-full'>
-        <div className='my-2'>
-          <label>Formula Name:</label>
-          <input
-            className='p-1 mx-8 ring-offset-2 ring-2'
-            placeholder='e.g., Enter Formula name'
-            value={formulaName}
-            onChange={(e) => setFormulaName(e.target.value)}
-          />
-        </div>
-        <div className='my-2'>
-          <label>Formula:</label>
-          <input
-            className='p-1 mx-20 ring-offset-2 ring-2'
-            placeholder='e.g., Enter a formula'
-            value={formula}
-            onChange={(e) => setFormula(e.target.value)}
-          />
-        </div>
-        <div className='my-2'>
-          <label>Number of Rows:</label>
-          <input
-            type="number"
-            className='p-1 mx-5 ring-offset-2 ring-2'
-            placeholder="Enter the number of rows"
-            onChange={handleNumRowsChange}
-          />
-        </div>
-        {template.map((item, index) => {
+    <div className='m-40'>
+      <div className='my-2'>
+        <p> <label>Name: {formulaName}</label></p>
+        <p> <label>Formula: {formula}</label></p>
+
+      </div>
+      {template.length > 0 && (
+        template.map((item) => {
           return (
-            <div key={index} style={{ padding: 10 }}>
-              {item.id === 0 ?
-               <div className="flex space-x-40  mx-20 ">
-                <label>Key</label>
-                <label>label</label>
-                <label>limit</label>
-                <label>unit</label>
-                <label>defaultValue</label>
-                <br/>
-                </div> 
-                : ""}
-               <input
-                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].key
-                    ? "ring-red-500"
-                    : ""
-                  }`}
-                onChange={(e) => updateValue(e, "key", item.id)}
-              />
-              {/* {renderError("key", index)} */}
-              <input
-                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].label
-                    ? "ring-red-500"
-                    : ""
-                  }`}
-                onChange={(e) => updateValue(e, "label", item.id)}
-              />
-              <input
-                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].limit
-                    ? "ring-red-500"
-                    : ""
-                  }`}
-                onChange={(e) => updateValue(e, "limit", item.id)}
-              />
-              <input
-                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].unit
-                    ? "ring-red-500"
-                    : ""
-                  }`}
-                onChange={(e) => updateValue(e, "unit", item.id)}
-              />
-              <input
-                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].defaultValue
-                    ? "ring-red-500"
-                    : ""
-                  }`}
-                onChange={(e) => updateValue(e, "defaultValue", item.id)}
-              />
+            <div key={item.key} className='p-2'>
+              <label className='my-2' key={item.key}>{item.label}</label> <input className='p-1 mx-2 ring-offset-2 ring-2'
+                placeholder={"Enter " + item.label} onChange={(e) => {
+                  textChangeEvent(e, item)
+                }} />
+              <label>{item.unit}</label>
+
             </div>
           )
-        })}
-      </div>
+        }))
+      }
       <div className='my-2'>
-        <button
+      <button
           className='border w-80 shadow-xl ring-offset-2 ring-2'
-          onClick={handleSubmit}
+          onClick={handleCalculate}
         >
           Submit
-        </button>
+        </button><br/><br/>
+        <label>Calculate Addition Value:{result}
+        </label> <br/><br/>
+        <label>Calculation :{multi}
+        </label>
       </div>
     </div>
-  );
+  )
 }
 
 export default FormulaForm;
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,240 @@
-import { useEffect, useState } from "react";
-//import Structure from './Structure'
-const Home = () => {
- const [sum, setSum] = useState(0);
- const [multi, setMulti] = useState(0);
-  const [template, setTemplate] = useState([])
-  const [formulaName,setFormulaName] = useState('')
-  const [formula,setFormula] = useState('')
-  useEffect(()=>{
-    const fetchFormula=async()=>{
-       let formulaTemplate =JSON.parse(localStorage.getItem("formulaTemplate"));
-            
-       let formulaName =localStorage.getItem("formula Name");
-       let formula =localStorage.getItem("formula");
-       setTemplate (formulaTemplate)
-       setFormulaName(formulaName)
-       setFormula(formula)
-    }
+import React, { useState } from "react";
+import * as XLSX from 'xlsx';
+import {useRouter} from 'next/router';
 
-    fetchFormula();
-  },[])
-  const textChangeEvent = (e, item) => {
-    template[item.id].value = parseFloat(e.target.value) ? parseFloat(e.target.value) : 0;
-    setTemplate(template)
-    console.log(JSON.stringify(template))
-    calculateSum();
-    calculateMulti();
-  }
-  // Use Object.values() to get an array of values from the JSON object
-  const value = Object.values(template);
-  // Calculate the sum of values and update the state
-  const calculateSum = () => {
-    const total = value.reduce((acc, curr) => (acc ? acc : 0) + curr.value, 0);
-    setSum(total);
+function Home() {
+  const [formulaName, setFormulaName] = useState('');
+  const [formula, setFormula] = useState('');
+  const [template, setTemplate] = useState([]);
+  const [errors, setErrors] = useState({ template: [] });
+  const [exportError, setExportError] = useState(null);
+  const router=useRouter();
+
+  const handleNumRowsChange = (event) => {
+    let tmpArr = [];
+    for (let i = 0; i < event.target.value; i++) {
+      let tmpObj = {
+        id: i,
+        key: '',
+        label: '',
+        limit: '',
+        unit: '',
+        defaultValue: '',
+        type: ''
+      }
+      tmpArr.push(tmpObj)
+    }
+    setTemplate(tmpArr);
+    console.log(tmpArr);
   };
-  const calculateMulti = () => {
-    const m = value.reduce((acc, curr) => (acc ? acc : 1) * curr.value, 0);
-    setMulti(m);
+
+  const updateValue = (e, updateFor, id) => {
+    console.log("Value" + e.target.value);
+    console.log('Update for' + updateFor);
+    console.log('id' + id);
+    template[id][updateFor] = e.target.value;
+    setTemplate([...template]);
+    validateTemplate();
+    console.log(JSON.stringify(template))
+  }
+
+  const validateTemplate = () => {
+    let valid = true;
+    const newTemplateErrors = [];
+
+    template.forEach((item, index) => {
+      const itemErrors = {};
+      if (!item.key.trim()) {
+        itemErrors.key = 'Key is required';
+        valid = false;
+      }
+      if (!item.label.trim()) {
+        itemErrors.label = 'Label is required';
+        valid = false;
+      }
+      if (!item.limit.trim()) {
+        itemErrors.limit = 'Limit is required';
+        valid = false;
+      }
+      if (!item.unit.trim()) {
+        itemErrors.unit = 'Unit is required';
+        valid = false;
+      }
+      if (!item.defaultValue.trim()) {
+        itemErrors.defaultValue = 'DefaultValue is required';
+        valid = false;
+      }
+      if (!item.type) {
+        itemErrors.type = 'Type is required';
+        valid = false;
+      }
+
+      newTemplateErrors.push(itemErrors);
+    });
+
+    setErrors({ ...errors, template: newTemplateErrors });
+    return valid;
+  }
+
+  const handleSubmit = () => {
+    const isTemplateValid = validateTemplate();
+
+    if (isTemplateValid && formulaName.trim() && formula.trim()) {
+      console.log("Form is valid");
+      localStorage.setItem("formulaTemplate", JSON.stringify(template));
+      localStorage.setItem("formula", formula);
+      localStorage.setItem("formula Name", formulaName);
+      router.push('/FormulaForm');
+    }
+  }
+
+  const options = [
+    { value: "date", label: "Date" },
+    { value: "string", label: "String" },
+    { value: "number", label: "Number" },
+    { value: "isFormula", label: "IsFormula" }
+  ];
+
+  const exportToExcel = () => {
+      const wb = XLSX.utils.book_new();
+      const wsData = [...template.map((item) => ({
+        Key: item.key,
+        Label: item.label,
+        Limit: item.limit,
+        Unit: item.unit,
+        DefaultValue: item.defaultValue,
+        Type: item.type
+      }))];
+      const ws = XLSX.utils.json_to_sheet(wsData);
+      XLSX.utils.book_append_sheet(wb, ws, 'Template Data');
+      const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+      const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'template.xlsx';
+      a.click();
+      URL.revokeObjectURL(url);
+      setExportError(null);
+  
   };
 
   return (
-    <div className='m-40'>
-      <div className='my-2'>
-        <p> <label>Name: {formulaName}</label></p>
-         <p> <label>Formula: {formula}</label></p>
-      
-      </div>
-      {template.length>0 &&  (
-        template.map((item) => {
+    <div className="m-5">
+      <div className='flex flex-col sm:w-full'>
+        <div className='my-2'>
+          <label>Formula Name:</label>
+          <input
+            className='p-1 mx-8 ring-offset-2 ring-2'
+            placeholder='e.g., Enter Formula name'
+            value={formulaName}
+            onChange={(e) => setFormulaName(e.target.value)}
+          />
+        </div>
+        <div className='my-2'>
+          <label>Formula:</label>
+          <input
+            className='p-1 mx-20 ring-offset-2 ring-2'
+            placeholder='e.g., Enter a formula'
+            value={formula}
+            onChange={(e) => setFormula(e.target.value)}
+          />
+        </div>
+        <div className='my-2'>
+          <label>Number of Rows:</label>
+          <input
+            type="number"
+            className='p-1 mx-5 ring-offset-2 ring-2'
+            placeholder="Enter the number of rows"
+            onChange={handleNumRowsChange}
+          />
+        </div>
+        {template.map((item, index) => {
           return (
-            <div key={item.key} className='p-2'>
-              <label className='my-2' key={item.key}>{item.label}</label> <input className='p-1 mx-2 ring-offset-2 ring-2'
-                placeholder={"Enter "+item.label} onChange={(e) => {
-                  textChangeEvent(e, item)
-                }} />
-                <label>{item.unit}</label>
-
+            <div key={index} style={{ padding: 10 }}>
+              {item.id === 0 ?
+                <div className="flex space-x-40  mx-20 ">
+                  <label>Key</label>
+                  <label>Label</label>
+                  <label>Limit</label>
+                  <label>Unit</label>
+                  <label>DefaultValue</label>
+                  <label>Type</label>
+                  <br />
+                </div>
+                : ""}
+              <input
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].key
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                onChange={(e) => updateValue(e, "key", item.id)}
+              />
+              <input
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].label
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                onChange={(e) => updateValue(e, "label", item.id)}
+              />
+              <input
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].limit
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                onChange={(e) => updateValue(e, "limit", item.id)}
+              />
+              <input
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].unit
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                onChange={(e) => updateValue(e, "unit", item.id)}
+              />
+              <input
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].defaultValue
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                onChange={(e) => updateValue(e, "defaultValue", item.id)}
+              />
+              <select
+                className={`p-1 m-2 ring-offset-2 ring-2 ${errors.template[index] && errors.template[index].type
+                  ? "ring-red-500"
+                  : ""
+                  }`}
+                value={item.type}
+                onChange={(e) => updateValue(e, "type", item.id)}
+              >
+                <option value="">Select an option</option>
+                {options.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
             </div>
           )
-        }) )
-      }
+        })}
+      </div>
       <div className='my-2'>
-        {/* <label>Calculate Addition Value:{sum}
-        </label> <br/> */}
-        <label>Calculation :{multi}
-        </label> 
+        <button
+          className='border w-80 shadow-xl ring-offset-2 ring-2'
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div><br/>
+      <div>
+      <button
+        className='border w-80 shadow-xl ring-offset-2 ring-2'
+        onClick={exportToExcel}
+      >
+        Export to Excel
+      </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
